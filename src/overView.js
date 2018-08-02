d6c0ea6075a19d2c22ccc930b613e0df357ae7af
Gilang Al Khansa <gilang.alkhansa@iproperty.com>
2018-08-03 01:26:30 +0700

    Create container page

--- src/overView.js
+++ src/overView.js
@@ -2,19 +2,62 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Button} from 'antd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const LeftWrapper = styled.div`
+  flex: 50%;
+`;
+
+const RightWrapper = styled.div`
+  flex: 50%;
+`;
+
+const FullWrapper = styled.div`
+  flex: 100%;
+`;
+
 
 class overView extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        <Button type="primary">Button</Button> Ant Design Button with primary color defined in ant-theme-vars.less 
-        </p>
-      </div>
+      <Wrapper>
+        <LeftWrapper>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+          <Button type="primary">Button</Button> Ant Design Button with primary color defined in ant-theme-vars.less 
+          </p>
+        </LeftWrapper>
+        <RightWrapper>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+          <Button type="primary">Button</Button> Ant Design Button with primary color defined in ant-theme-vars.less 
+          </p>
+        </RightWrapper>
+        <FullWrapper>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+          <Button type="primary">Button</Button> Ant Design Button with primary color defined in ant-theme-vars.less 
+          </p>
+
+        </FullWrapper>
+      </Wrapper>
     );
   }
 }
