a09e93dd211ceaa31c1a4c5a2f7e633e8250a70c
Gilang Al Khansa <gilang.alkhansa@iproperty.com>
2018-08-02 18:07:01 +0700

    Setup react router and overView page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Button} from 'antd';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import overView from './overView';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        <Button type="primary">Button</Button> Ant Design Button with primary color defined in ant-theme-vars.less 
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={overView} />
+          {/* <Route exact path="/dynamic" component={AsyncDynamicPAge} /> */}
+          {/* <Route component={AsyncNoMatch} /> */}
+        </Switch>
+      </Router>
     );
   }
 }
